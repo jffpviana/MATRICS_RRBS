d74ca6a42ba4f524c13d06f3434c09df0e2d36e2
Joana Viana <j.f.viana@exeter.ac.uk>
2021-04-12 15:20:30 +0100

    raw coverage plot CTR HC

--- analysis/CTR/HC/2.coverage_plots.r
+++ analysis/CTR/HC/2.coverage_plots.r
@@ -5,8 +5,13 @@ library(BiSeq)
 
 input_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/HC/methylation/"
 output_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/HC/analysis/"
+plots_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/HC/QC/plots/"
 region <- "HC_ID"
 cohort <- "CTR"
 
 
 load(file=paste0(output_dir, "biseq_rawmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load BiSeq object
+
+pdf(paste0(plots_dir, "coverage_boxplots", str_replace(region, "_ID", ""),".pdf")) #start pdf file
+covBoxplots(data_meth, col = "blue", las = 2)
+dev.off()
