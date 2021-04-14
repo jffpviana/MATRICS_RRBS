08b4f27bbb4f84e305a79e0570a78a598730e1e4
Joana Viana <j.f.viana@exeter.ac.uk>
2021-04-14 11:48:55 +0100

    annotate biseq object

--- analysis/CTR/HC/2.annotate_biseqobj_rat.r
+++ analysis/CTR/HC/2.annotate_biseqobj_rat.r
@@ -2,7 +2,6 @@
 library("TxDb.Rnorvegicus.UCSC.rn6.refGene") # load genome specific library
 input_dir <- "/home/vianaj/Documents/MATRICS/analysis/CTR/HC/analysis/"
 anno_dir <- "/home/vianaj/Documents/MATRICS/analysis/CTR/"
-output_dir <- "/home/vianaj/Documents/MATRICS/analysis/CTR/HC/methylation/"
 genome <- "rn6"
 current_anno <- "annotations_rn6_130421.RData"
 region <- "HC_ID"
@@ -31,17 +30,11 @@ seqlevels(data_meth@rowRanges)[grep('Y', seqlevels(data_meth@rowRanges))] <- str
 scaffolds <- seqlevels(data_meth@rowRanges)[grep('[.]1', seqlevels(data_meth@rowRanges))] # The unplaced scaffold have a '.1' at the end of the chromosome name. In the annotation file they have a 'chrUn_' at the start and 'v1' at the end. Replace '.1' for 'v1
 
 #Look for each scaffold in the annotation object and replace its name with the one in the annotation object
+for(i in scaffolds){
+	new_scaffold <- seqlevels(annotations)[grep(str_replace(i, '[.]1', ''), seqlevels(annotations))] # look for same name in annotations file
+	seqlevels(data_meth@rowRanges)[grep(i, seqlevels(data_meth@rowRanges))] <- new_scaffold # replace in the data object
+}
 
+temp_annotated <- annotate_regions(regions = data_meth@rowRanges, annotations = annotations, ignore.strand = TRUE, quiet = FALSE)
 
-
-
-seqlevels(data_meth@rowRanges)[grep('[.]1', seqlevels(data_meth@rowRanges))] <- str_replace(seqlevels(data_meth@rowRanges)[grep('[.]1', seqlevels(data_meth@rowRanges))], '[.]1', 'v1') '
-
-seqlevels(data_meth@rowRanges)[grep('v1', seqlevels(data_meth@rowRanges))] <- paste0("chrUn_", seqlevels(data_meth@rowRanges)[grep('v1', seqlevels(data_meth@rowRanges))]) #place 'chrUn_' at the start of the scaffold name
-
-
-temp_annotated <- annotate_regions(regions = data_meth@rowRanges, annotations = annotations, ignore.strand = TRUE, quiet = FALSE) #
-
-	save(temp_annotated, file=paste0(output_dir, "annotated_cov_",  str_replace(list_cov[i], ".cov", ""), "RData"))
-
-####STOPPED HERE, GENERATED ANNOTATION FOR EACH FILE SPARATE, BUT HOW WILL I INPUT INTO BISEQ? MAYBE IT'S BETTER TO DO THIS AFTER MERGING THE COV FILES.###
+save(data_meth, file=paste0(input_dir, "biseq_anno_meth_obj_", cohort, "_", str_replace(region, "_ID", ""), ".RData"))
