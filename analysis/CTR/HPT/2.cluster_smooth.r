9d2e64ffa6c1b00b5d1e4ee0926c1cd1d851913d
Joana Viana <j.f.viana@exeter.ac.uk>
2021-05-07 12:49:30 +0100

    HPT clustering

---
+++ analysis/CTR/HPT/2.cluster_smooth.r
@@ -0,0 +1,36 @@
+library(stringr) #load libraries
+library(plyr)
+library(data.table)
+library(BiSeq)
+
+
+args <- commandArgs(TRUE) # option to read arguments from bash script
+args[1]->input_dir
+args[2]->output_dir
+args[3]->region
+args[4]->cohort
+
+
+load(file=paste0(input_dir, "biseq_rawmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load BiSeq object
+
+dim(data_meth)
+# 3322735      24
+
+
+rrbs.clust.unlim <- clusterSites(object = data_meth,
+                                  groups = colData(data_meth)$Group,
+                                  perc.samples = 0.5,
+                                  min.sites = 5,
+                                  max.dist = 500)  #searches for agglomerations of CpG sites across all samples
+
+#In the smoothing step CpG sites with high coverages get high weights. To reduce bias due to unusually high coverages we limit the coverage
+
+ind.cov <- totalReads(rrbs.clust.unlim) > 0
+quant <- quantile(totalReads(rrbs.clust.unlim)[ind.cov], 0.9)
+quant
+rrbs.clust.lim <- limitCov(rrbs.clust.unlim, maxCov = quant)
+
+#We then smooth the methylation values of CpG sites within the clusters with the default bandwidth h = 80 base pairs.
+predictedMeth <- predictMeth(object = rrbs.clust.lim, mc.cores=8)
+
+save(rrbs.clust.unlim, rrbs.clust.lim, predictedMeth, file=paste0(output_dir, "biseq_predictedmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #save BiSeq object
---
+++ analysis/CTR/HPT/2.cluster_smooth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --ntasks 10
+#SBATCH --time 1-0:0:0
+#SBATCH --qos castles
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+module load R-bundle-Bioconductor/3.10-foss-2019b-R-3.6.2
+
+export R_LIBS_USER=${HOME}/R/library/${EBVERSIONR}/${BB_CPU}
+
+cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/jobs/CTR/PFC/
+
+input_dir="/rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/PFC/analysis/"
+output_dir="//rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/PFC/analysis/"
+region="PFC_ID"
+cohort="CTR"
+
+Rscript 2.clusters.r $input_dir $output_dir $region $cohort
