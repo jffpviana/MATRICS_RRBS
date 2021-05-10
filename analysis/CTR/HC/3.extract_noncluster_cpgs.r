6e17191c4b6133f54177376ca26b809dcdd66dbe
Joana Viana <j.f.viana@exeter.ac.uk>
2021-05-10 17:16:24 +0100

    calculate differentially methylated positions script

--- analysis/CTR/HC/3.extract_noncluster_cpgs.r
+++ analysis/CTR/HC/3.extract_noncluster_cpgs.r
@@ -9,12 +9,12 @@ output_dir="/home/vianaj/Documents/MATRICS/analysis/CTR/HC/analysis/"
 region="HC_ID"
 cohort="CTR"
 
-load(file=paste0(output_dir, "biseq_rawmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load raw BiSeq object
+load(file=paste0(input_dir, "biseq_rawmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load raw BiSeq object
 
-load(file=paste0(output_dir, "biseq_predictedmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load clustered/smooth BiSeq objects
+load(file=paste0(input_dir, "biseq_predictedmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load clustered/smooth BiSeq objects
 
 
-rrbs.raw.reduced <- filterBySharedRegions(object=data_meth, groups=colData(data_meth)$Group, perc.samples=0.5, minCov=10)
+rrbs.raw.reduced <- filterBySharedRegions(object=data_meth, groups=colData(data_meth)$Group, perc.samples=0.5, minCov=10) #remove low coverage and lowly represented sites, keep consistent with what was used in the clustering steps
 
 dim(data_meth)
 #3322735      24
@@ -22,5 +22,32 @@ dim(data_meth)
 dim(rrbs.raw.reduced)
 #1047652      24
 
-#convert BSRaw to BSRel object
-rrbs.raw.rel <- rawToRel(rrbs.raw.reduced) # convert to methylation values
+#convert BSRaw to BSRel object (coverage to methylation values)
+rrbs.raw.rel <- rawToRel(rrbs.raw.reduced)
+
+RRBS_rawbetas <- as.data.frame(methLevel(rrbs.raw.rel)) #extract raw methylation values
+dim(RRBS_rawbetas)
+# 1047652      24
+
+coordinates_raw <- as.data.frame(rrbs.raw.rel@rowRanges) # extract CpG positions
+rownames(RRBS_rawbetas) <- paste(coordinates_raw$seqnames, ":", coordinates_raw$start, sep="") #add as rownames of methylation values
+
+
+RRBS_smoothbetas <- as.data.frame(methLevel(predictedMeth)) #extract smoothed methylation values
+dim(RRBS_smoothbetas)
+# 780681     24
+
+coordinates_pred <- as.data.frame(predictedMeth@rowRanges)
+rownames(RRBS_smoothbetas) <- paste(coordinates_pred$seqnames, ":", coordinates_pred$start, sep="") #add as rownames of methylation values #add as rownames of methylation values
+
+reduced_RRBS_rawbetas <- RRBS_rawbetas[-match(rownames(RRBS_smoothbetas), rownames(RRBS_rawbetas)),] #excluse sites that are present in the smoothed data frame
+
+
+all_betas <- rbind(reduced_RRBS_rawbetas, RRBS_smoothbetas) #join raw and smoother betas
+dim(all_betas)
+#1047652      24
+
+all_betas_order <- all_betas[order(rownames(all_betas)),] #order by coordinate
+
+
+write.csv(all_betas_order, file=paste0(output_dir, "complete_betas_", cohort, "_", str_replace(region, "_ID", ""))) #save complete betas
