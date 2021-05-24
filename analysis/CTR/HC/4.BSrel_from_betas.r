88ec37ba03dfb3eb4cdebd3244b2530251e50b2d
Joana Viana <j.f.viana@exeter.ac.uk>
2021-05-24 15:09:02 +0100

    create BSrel obj from all betas

--- analysis/CTR/HC/4.BSrel_from_betas.r
+++ analysis/CTR/HC/4.BSrel_from_betas.r
@@ -17,18 +17,12 @@ meth_betas <- read.csv(paste0(input_dir, "complete_betas_", cohort, "_", str_rep
 pheno <- read.csv(pheno_path, stringsAsFactors=FALSE) #load phenotype file
 
 
-identical(pheno[, region], colnames(meth_betas)[1:nrow(pheno)])
+identical(pheno[, region], colnames(meth_betas)[1:nrow(pheno)]) # check the samples are in the same order
 
+rowRanges <- makeGRangesFromDataFrame(meth_betas[,-(1:(nrow(pheno)))])
 
+betas_all_obj<- BSrel(colData=pheno, methLevel=as.matrix(meth_betas[,1:nrow(pheno)]), rowRanges=rowRanges) # create a BSrel object from all the betas
 
+colData(betas_all_obj)$Group<-factor(colData(betas_all_obj)$Group, levels=c('low', 'inter', 'high'))
 
-
-
-BSrel(metadata=pheno, meth_betas)
-
-
-
-betaResults <- betaRegression(formula = ~group,
-link = "probit",
-object = meth_betas,
-type = "BR")
+save(betas_all_obj, file=paste0(output_dir, "betas_all_obj_", cohort, "_", str_replace(region, "_ID", ""))) #save BiSeq object
