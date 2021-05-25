e69c2535ad39d1b424abb53c1a914992d9d0e582
Joana Viana <j.f.viana@exeter.ac.uk>
2021-05-25 14:09:24 +0100

    DMPs script for running in background BEAR

--- analysis/CTR/HC/5.DMPs.r
+++ analysis/CTR/HC/5.DMPs.r
@@ -4,20 +4,21 @@ library(data.table)
 library(BiSeq)
 library(parallel)
 
-
-input_dir <- "/home/vianaj/Documents/MATRICS/analysis/CTR/HC/analysis/"
-output_dir <- "/home/vianaj/Documents/MATRICS/analysis/CTR/HC/analysis/"
-pheno_path <- "/home/vianaj/Documents/MATRICS/analysis/CTR/phenoCTR.csv"
-function_path <- "/home/vianaj/Documents/MATRICS/MATRICS_repo/analysis/betaRegression3Groups.r"
+args <- commandArgs(TRUE) # option to read arguments from bash script
+args[1]->input_dir
+args[2]->output_dir
+args[3]->pheno_path
+args[4]->function_path
+args[5]->region
+args[6]->cohort
 
 source(function_path)
 numcores <- detectCores()
-
-region="HC_ID"
-cohort="CTR"
 
 load(paste0(input_dir, "betas_all_obj_", cohort, "_", str_replace(region, "_ID", ""))) #save BiSeq object
 
 group <- colData(betas_all_obj)$Group
 
-betaResults <- betaRegression3Groups(formula = ~group, link = "probit", object = betas_all_obj, type = "BR", mc.cores=numcores)
+betaResults <- betaRegression3Groups(formula = ~group, link = "probit", object = betas_all_obj, type = "BR", mc.cores=10)
+
+write.csv(betaResults, file=paste0(output_dir, "DMPs_", cohort, "_", str_replace(region, "_ID", ""), ".csv"))
