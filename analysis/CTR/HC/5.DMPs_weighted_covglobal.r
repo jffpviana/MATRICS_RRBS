aafb5604d26b1b4d0087a854b60543b57a336895
Joana Viana <j.viana@bham.ac.uk>
2021-06-07 14:35:28 +0100

    try different weights based on coverage of sites

---
+++ analysis/CTR/HC/5.DMPs_weighted_covglobal.r
@@ -0,0 +1,35 @@
+library(stringr) #load libraries
+library(plyr)
+library(data.table)
+library(BiSeq)
+library(parallel)
+
+args <- commandArgs(TRUE) # option to read arguments from bash script
+args[1]->input_dir
+args[2]->output_dir
+args[3]->pheno_path
+args[4]->function_path
+args[5]->region
+args[6]->cohort
+
+
+source(function_path) #load the beta regression function
+
+numcores <- detectCores() #detect number of cores available for parallel
+
+load(paste0(input_dir, "betas_all_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load BiSeq object
+
+load(file=paste0(input_dir, "biseq_rawmeth_obj_", cohort, "_", str_replace(region, "_ID", ""))) #load raw BiSeq object
+
+group <- colData(betas_all_obj)$Group #create variable with groups to test
+
+coverage <- totalReads(data_meth) #extract coverage and use chromosome:position as rownames
+rownames(coverage) <- paste0(as.character(data.frame(rowRanges(data_meth))$seqnames), ":", as.character(data.frame(rowRanges(data_meth))$start))
+
+global_max <- max(coverage) #calculate the maximum coverage of all CpGs
+
+weight_cov <- apply(coverage, 2, function(x) (x*10)/global_max)
+
+betaResults <- betaRegressionWeights(formula = ~group, link = "probit", object = betas_all_obj, weights=weight_cov,  type = "BR", mc.cores=numcores) #perform weithed beta regression
+
+write.csv(betaResults, file=paste0(output_dir, "DMPs_weighted_global_", cohort, "_", str_replace(region, "_ID", ""), ".csv")) #write out results
---
+++ analysis/betaRegression_weighted.r
@@ -0,0 +1,137 @@
+# Adapted Beta Regression function from Biseq (https://rdrr.io/bioc/BiSeq/src/R/betaRegression.R)
+
+
+library(BiSeq)
+library(betareg)
+library(lmtest) # to use likelihood-ratio test (lrt)
+
+
+
+betaRegressionWeights <- function(formula, link = "probit", object, mc.cores, weights, ...){
+
+  strand(object) <- "*"
+  object <- sort(object)
+
+  if(link == "loglog"){
+    inv.link <- function(x){
+      exp(-exp(-x))
+    }
+  }
+  if(link == "logit"){
+    inv.link <- function(x){
+      1 / ( 1 + exp(-x))
+    }
+  }
+  if(link == "probit"){
+    inv.link <- function(x){
+      pnorm(x)
+    }
+  }
+  if(link == "cloglog"){
+    inv.link <- function(x){
+      1 - exp(-exp(x))
+    }
+  }
+  if(link == "log"){
+    inv.link <- function(x){
+      exp(x)
+    }
+  }
+
+  min.meth <- min(methLevel(object)[methLevel(object) > 0], na.rm=TRUE)
+  max.meth <- max(methLevel(object)[methLevel(object) < 1], na.rm=TRUE)
+
+  object.split <- split(object, f = rep(1:mc.cores, length.out = nrow(object)))
+
+  beta.regr <- function(object.part, formula, link, weights, ...){
+    chr <- as.character(seqnames(rowRanges(object.part)))
+    pred.meth.part <- methLevel(object.part)
+    weights.part <- weights[rownames(pred.meth.part),]
+    p.val1 <- rep(NA,nrow(object.part))
+    p.val2 <- rep(NA,nrow(object.part))
+    meth.group1 <- rep(NA,nrow(object.part))
+    meth.group2 <- rep(NA,nrow(object.part))
+    meth.group3 <- rep(NA,nrow(object.part))
+    meth.diff1 <- rep(NA,nrow(object.part))
+    meth.diff2 <- rep(NA,nrow(object.part))
+    direction <- rep(NA,nrow(object.part))
+    pseudo.R.sqrt <- rep(NA,nrow(object.part))
+    estimate1 <- rep(NA,nrow(object.part))
+    std.error1 <- rep(NA,nrow(object.part))
+    estimate2 <- rep(NA,nrow(object.part))
+    std.error2 <- rep(NA,nrow(object.part))
+
+    for(j in 1:nrow(pred.meth.part)){
+      pred.meth <- pred.meth.part[j,]
+      weights.loop <- weights.part[j,]
+      pred.meth[pred.meth == 0] <- min.meth
+      pred.meth[pred.meth == 1] <- max.meth
+      data <- cbind(pred.meth = pred.meth,
+                    as.data.frame(colData(object)))
+      options(show.error.messages = FALSE)
+      suppressWarnings(
+                       lmodel <- try(summary(betareg(formula = as.formula(paste("pred.meth ~ ", formula[2])),
+                                                     data=data, link = link, weights = weights.loop, ...)), silent=TRUE)
+                       )
+      options(show.error.messages = TRUE)
+      if((class(lmodel) == "try-error")){
+        p.val1[j] <- NA
+        meth.diff1[j] <- NA
+        p.val2[j] <- NA
+        meth.diff2[j] <- NA
+      } else{
+        if(!lmodel$converged){
+	  p.val1[j] <- NA
+	  meth.diff1[j] <- NA
+    p.val2[j] <- NA
+	  meth.diff2[j] <- NA
+	} else{
+                                        # Test auf 0: 2 * pnorm(-abs(Estimate / Std.Error))
+                                        # Test auf min.diff > 0: 2 * min(0.5, pnorm( -(abs(Estimate)-min.diff)/Std.Error, mean=-min.diff))
+          p.val1[j] <- max(lmodel$coefficients$mean[2, 4], 1e-323) # should not be zero
+          p.val2[j] <- max(lmodel$coefficients$mean[3, 4], 1e-323) # should not be zero
+          baseline <- lmodel$coefficients$mean[1, 1]
+          coef1 <- lmodel$coefficients$mean[2, 1]
+          se1 <- lmodel$coefficients$mean[2, 2]
+          coef2 <- lmodel$coefficients$mean[3, 1]
+          se2 <- lmodel$coefficients$mean[3, 2]
+          meth.group1[j] <- inv.link(baseline)
+          meth.group2[j] <- inv.link(baseline + coef1)
+          meth.group3[j] <- inv.link(baseline + coef2)
+          meth.diff1[j] <-  meth.group2[j] - meth.group1[j]
+          meth.diff2[j] <-  meth.group3[j] - meth.group1[j]
+          pseudo.R.sqrt[j] <- lmodel$pseudo.r.squared
+          estimate1[j] <- coef1
+          std.error1[j] <- se1
+          estimate2[j] <- coef2
+          std.error2[j] <- se2
+        }
+      }
+    }
+
+    out <- data.frame(chr = chr,
+                      pos = start(ranges(object.part)),
+                      meth.group1,
+                      meth.group2,
+                      meth.group3,
+                      meth.diff1,
+                      meth.diff2,
+                      estimate1,
+                      std.error1,
+                      p.val1,
+                      estimate2,
+                      std.error2,
+                      p.val2,
+                      pseudo.R.sqrt)
+    return(out)
+  }
+
+  summary.df.l <- mclapply(object.split, beta.regr, formula=formula, link=link, mc.cores=mc.cores, weights=weights, ...)
+  summary.df <- do.call(rbind, summary.df.l)
+  pos.object <- paste(seqnames(object), start(object), sep="_")
+  pos.summary <- paste(summary.df$chr, summary.df$pos, sep="_")
+  ind <- match(pos.object, pos.summary)
+  summary.df <- summary.df[ind,]
+  summary.df$cluster.id <- mcols(rowRanges(object))$cluster.id
+  return(summary.df)
+}
