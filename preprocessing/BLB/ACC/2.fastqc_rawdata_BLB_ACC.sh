1df85313e9e5784bd62ad8ffe7d012f6934c2cb6
Joana Viana <j.viana@bham.ac.uk>
2021-07-05 16:11:51 +0100

    change queue from castles to default

--- preprocessing/BLB/ACC/2.fastqc_rawdata_BLB_ACC.sh
+++ preprocessing/BLB/ACC/2.fastqc_rawdata_BLB_ACC.sh
@@ -13,4 +13,4 @@ module load FastQC/0.11.9-Java-11 #load the module fastqc
 mkdir /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/fastqc
 
 cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/ #change directory
-fastqc *.fq -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/fastqc/ # run fastqc for all samples and output in the directory of the respective cohort
+fastqc *.fq -o   /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/fastqc/ # run fastqc for all samples and output in the directory of the respective cohort
