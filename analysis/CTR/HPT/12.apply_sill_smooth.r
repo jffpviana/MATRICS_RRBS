7ee3f0a9bc5ad36e5c5eb91e89318196b323611f
Joana Viana <j.viana@bham.ac.uk>
2021-08-05 10:54:45 +0100

    DMRs HPT

---
+++ analysis/CTR/HPT/12.apply_sill_smooth.r
@@ -0,0 +1,44 @@
+library(stringr) #load libraries
+library(plyr)
+library(data.table)
+library(BiSeq)
+library(parallel)
+
+
+input_dir="/home/vianaj/Documents/MATRICS/analysis/CTR/HPT/analysis/"
+output_dir="/home/vianaj/Documents/MATRICS/analysis/CTR/HPT/analysis/"
+pheno_path <- "/home/vianaj/Documents/MATRICS/analysis/CTR/phenoCTR.csv"
+
+region<-"HPT_ID"
+cohort<-"CTR"
+
+#####change this values based on the variogram plotted in the previous script #####
+#in cases where the variogram goes beyond 1, set sill at 1.
+sill1 <- 1
+sill2 <- 1
+#####
+
+load(paste0(input_dir, "Variograms_", cohort, "_", str_replace(region, "_ID", ""), ".RData")) # load the R object with the two varigrams
+
+
+#smooth using sills determined previously from variogram (previous script)
+vario1.sm <- smoothVariogram(vario1, sill = sill1)
+
+vario2.sm <- smoothVariogram(vario2, sill = sill2)
+
+
+#plot the two variograms
+pdf(paste0(output_dir, "Variograms_sill_", cohort, "_", str_replace(region, "_ID", ""), ".pdf"))
+
+  plot(vario1$variogram$v, main="low vs inter")
+  lines(vario1.sm$variogram[,c("h", "v.sm")],
+  col = "red", lwd = 1.5)
+ ## results of interest:
+  plot(vario2$variogram$v, main="low vs inter")
+  lines(vario2.sm$variogram[,c("h", "v.sm")],
+  col = "red", lwd = 1.5)
+
+dev.off()
+
+#save the smoothed varigrams
+save(vario1.sm, vario2.sm, file=paste0(output_dir, "Variograms_smooth_", cohort, "_", str_replace(region, "_ID", ""), ".RData"))
